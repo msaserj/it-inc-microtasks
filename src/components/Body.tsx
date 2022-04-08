2d65ba9edc9b5d1f3937728cdedd2c8ea04c4fa3
msaserj <serg.ks@gmail.com>
2022-04-08 23:30:01 +0300

    microtasks 03 video for Button

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Button} from "./Button";
 
 
 type BodyType = {
@@ -6,10 +7,21 @@ type BodyType = {
 }
 
 export const Body = (props: BodyType) => {
-    return(
+
+    const Button01Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+
+    const Button02Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+
+    return (
+
         <div>
             <h1>{props.titleForBody}</h1>
-
+            <Button name={"Button01"} callBack={() => Button01Foo('I`m Vasya', 21)}/>
+            <Button name={"Button02"} callBack={() => Button02Foo('I`m Vanya', 22)}/>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,33 @@ type HeaderType = {
     titleForHeader: string
 }
 
-const setActive = ({isActive}: {isActive: boolean}) => isActive ? css.activeLink : css.inactiveLink;
+const setActive = ({isActive}: { isActive: boolean }) => isActive ? css.activeLink : css.inactiveLink;
 
 
 export const Header = (props: HeaderType) => {
-    return(
+
+    // const firstSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hello")
+    // }
+    // const secondSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hi")
+    // }
+
+    const onClickHandler = (name: string) => {
+        console.log(name)
+    }
+
+    return (
         <nav className={css.nav}>
             <h1>{props.titleForHeader}</h1>
 
-            <NavLink to="/" className = {setActive} >Body</NavLink>
-            <NavLink to="/topcars" className = {setActive} >TopCars</NavLink>
-            <NavLink to={"/money"} className = {setActive} >Money</NavLink>
+            <NavLink to="/" className={setActive}>Body</NavLink>
+            <NavLink to="/topcars" className={setActive}>TopCars</NavLink>
+            <NavLink to={"/money"} className={setActive}>Money</NavLink>
+            <div>
+                <button onClick={(event) => onClickHandler("VASYA")}>MyButton01</button>
+                <button onClick={(event) =>onClickHandler("VANYA")}>MyButton02</button>
+            </div>
 
         </nav>
     )
