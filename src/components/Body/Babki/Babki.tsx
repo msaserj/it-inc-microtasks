b40f1f1d3d87f62a3abd56cf8bc21f460c2b07c1
msaserj <serg.ks@gmail.com>
2022-04-24 12:29:48 +0300

    microtasks 05 filter refactor ul to table for Babki

--- src/components/Body/Babki/Babki.tsx
+++ src/components/Body/Babki/Babki.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Button} from "../Buttons/Button";
+import css from "../TopCars/TopCars.module.css";
 
 
 type FilterType = "All" | "Dollars" | "Rubls"
@@ -9,21 +10,23 @@ export const Babki = () => {
     const [money, setMoney] = useState([
         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
         {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Rubls', value: 100, number: ' w1234567890'},
         {banknots: 'Dollars', value: 100, number: ' e1234567890'},
         {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Rubls', value: 100, number: ' r1234567890'},
         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+        {banknots: 'Rubls', value: 50, number: ' v1234567890'},
     ])
 
     const [filter, setFilter] = useState<FilterType>("All")
 
     let currentMoney = money
     if(filter === "Dollars") {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === "Dollars")
+        currentMoney = money.filter(t => {
+            return t.banknots === "Dollars"
+        })
     } else if (filter === "Rubls") {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === "RUBLS")
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "Rubls")
     }
 
     const onClickFilterHandler = (nameButton: FilterType) => {
@@ -32,20 +35,28 @@ export const Babki = () => {
 
     return (
         <div>
-            <ul>
-                {currentMoney.map((t, index) => {
-                    return (
-                        <li key={index}>
-                            <span> {t.banknots} </span>
-                            <span> {t.value} </span>
-                            <span> {t.number} </span>
-                        </li>
-                    )
-                })}
-            </ul>
+            <h2>Babki!</h2>
             <Button name={"All"} callBack={() => onClickFilterHandler("All")}/>
             <Button name={"Rubls"} callBack={() => onClickFilterHandler("Rubls")}/>
             <Button name={"Dollars"} callBack={() => onClickFilterHandler("Dollars")}/>
+            <table className={css.color}>
+                <tbody>
+                <tr>
+                    <th>Banknots</th>
+                    <th>Value</th>
+                    <th>Serial number</th>
+                </tr>
+                {currentMoney.map((t, index) => {
+                    return (
+                        <tr key={index}>
+                            <td> {t.banknots} </td>
+                            <td> {t.value} </td>
+                            <td> {t.number} </td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
         </div>
     );
 };
