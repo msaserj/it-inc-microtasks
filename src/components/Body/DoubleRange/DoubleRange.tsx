2920be9bf63b24c57cca0ae79b30c4bb7c344083
msaserj <serg.ks@gmail.com>
2022-08-21 23:54:22 +0300

    DoubleInput from library rc-slider

--- src/components/Body/DoubleRange/DoubleRange.tsx
+++ src/components/Body/DoubleRange/DoubleRange.tsx
@@ -3,20 +3,28 @@ import React, {useState} from 'react';
 import 'rc-slider/assets/index.css';
 
 import {TwoThumbInputRange} from "react-two-thumb-input-range";
-import CarSlider from "./DoubleRangeTwo";
+import {DoubleRangeTwo} from "./DoubleRangeTwo";
+
 
 
 export const DoubleRange = () => {
 
-    const [value, setValue] = useState<[number,number]>([1000, 4333])
+    const [value, setValue] = useState<[number,number]>([0, 1000])
     const onValueChange = (values: [number,number]) => {
         setValue(values)
     }
     return (
             <div>
                 <h2>InputRange</h2>
-                <TwoThumbInputRange onChange={onValueChange} values={value} min={1000} max={10000} />
-                <CarSlider/>
+                <div >
+                    <p>Eritrean Nakfa:</p>
+                    <p>
+                        {value[0]}Nfk - {value[1]}Nfk
+
+                    </p>
+                </div>
+                <TwoThumbInputRange  onChange={onValueChange} values={value} min={0} max={1000} />
+                <DoubleRangeTwo/>
             </div>
     );
 };
--- src/components/Body/DoubleRange/DoubleRangeTwo.tsx
+++ src/components/Body/DoubleRange/DoubleRangeTwo.tsx
@@ -1,42 +1,42 @@
-import React, { Component } from "react";
-// import "./styles.css";
+import React, {useState} from "react";
+import css from './DoubleRangeTwo.module.css'
 import Range from "rc-slider";
 import "rc-slider/assets/index.css";
 
-class CarSlider extends Component {
-    state = { sliderValues: [0, 100] };
+export const DoubleRangeTwo = () => {
+    const sliderValues = [0, 100]
 
-    handleChange = (sliderValues: any) => {
-        this.setState({ sliderValues });
+    const [valueRange, setValueRange] = useState<any>(sliderValues)
+
+    const handleChange = (sliderValues: any) => {
+        setValueRange(sliderValues);
     };
 
-    render() {
-        const { sliderValues } = this.state;
+    return (
+        <div className={css.range}>
+            <div >
+                <p>Eritrean Nakfa:</p>
+                <p>
+                    {valueRange[0]}Nfk - {valueRange[1]}Nfk
 
-        return (
-            <div className="mka__range-alignment">
-                <div className="mka__range-text">
-                    <p>Preisklasse:</p>
-                    <p>
-                        {sliderValues[0]}€ - {sliderValues[1]}€
-                    </p>
-                </div>
-                <div className="mka__range-btn-align">
-                    <div className="mka__range-width">
-                        <Range
-                            range
-                            allowCross={false}
-                            onChange={this.handleChange}
-                            defaultValue={[40, 60]}
-                            min={0}
-                            max={100}
-                        />
-                    </div>
-                    <button className="mka__range-button">Filter</button>
+                </p>
+            </div>
+            <div className="mka__range-btn-align">
+                <div className="sliderArea">
+                    <Range
+                        onChange={handleChange}
+                        range
+                        allowCross={false}
+                        marks={{
+                            0: `$ 0`,
+                            100: `$ 100`
+                        }}
+                        min={0}
+                        max={100}
+                        defaultValue={[40, 60]}
+                    />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default CarSlider;
